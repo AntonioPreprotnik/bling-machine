2522c8db481b7c877730eafd5225da5676206cf3
pozhega <mpozega94@gmail.com>
2022-03-03 18:42:55 +0100

    Remove unused file

--- src/backend/app/interceptors.clj
+++
@@ -1,9 +0,0 @@
-(ns app.interceptors
-  (:require
-   [xiana.core :as xiana]))
-
-(def sample-app-controller-interceptor
-  {:enter (fn [state]
-            (xiana/ok state))
-   :leave (fn [state]
-            (xiana/ok state))})
