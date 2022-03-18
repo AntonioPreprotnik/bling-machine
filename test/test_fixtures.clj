955dd51f0400d970afb4dbd91f3bdee59e07bfa4
enesj <enes.jakic@gmail.com>
2022-03-18 13:52:06 +0100

    Excluded 'seeds' table from 'clean-db' query in 'test-fixtures'.

--- test/test_fixtures.clj
+++ test/test_fixtures.clj
@@ -14,6 +14,7 @@
                              FROM information_schema.tables
                              WHERE table_schema = 'public'
                              AND table_type = 'BASE TABLE'
-                             AND table_name != 'migrations')
+                             AND table_name != 'migrations'
+                             AND table_name != 'seeds')
                          || ' CASCADE';
                        END; $$;"])))
