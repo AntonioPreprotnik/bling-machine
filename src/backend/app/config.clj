3eb746bdb24d295288cf91469cca191cf7af68a4
enesj <enes.jakic@gmail.com>
2022-04-25 11:31:57 +0200

    Applied ONG PR: https://github.com/VeryBigThings/onlygems-web-platform/pull/73 Moved `shadow-cljs` to `devDependencies` in `package.json`

--- src/backend/app/config.clj
+++ src/backend/app/config.clj
@@ -4,6 +4,24 @@
             [com.walmartlabs.dyn-edn :refer [env-readers]]
             [xiana.commons :refer [deep-merge]]))
 
+(defn- parse-db-url [db-url]
+  (re-find (re-matcher #"postgres://(\w+):(\w+)@([\w\-\.]+):(\d+)/(\w+)" db-url)))
+
+(defn- db-url-to-args [config]
+  (let [db-url (get-in config [:framework.db.storage/postgresql :dburl])
+        [_ user password host port dbname] (parse-db-url db-url)]
+    (assoc config :framework.db.storage/postgresql (merge (:framework.db.storage/postgresql config)
+                                                          {:user     user
+                                                           :password password
+                                                           :host     host
+                                                           :port     (Integer/parseInt port)
+                                                           :dbname   dbname}))))
+
+(defn transform-db-config [config]
+  (if (get-in config [:framework.db.storage/postgresql :dburl])
+    (db-url-to-args config)
+    config))
+
 (defn- read-default-config []
   (->> (io/resource "default.edn")
        (slurp)
@@ -15,6 +33,7 @@
        (edn/read-string {:readers (env-readers)})))
 
 (defn load-config [app-config]
-  (deep-merge (read-default-config)
-              (read-env-config)
-              app-config))
+  (->> (deep-merge (read-default-config)
+                   (read-env-config)
+                   app-config)
+       transform-db-config))
