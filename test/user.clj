0300e37b28cde2ec864556fc1fa5e99f91d29339
enesj <enes.jakic@gmail.com>
2022-04-28 12:23:30 +0200

    Added more examples to test/user.clj.

--- test/user.clj
+++ test/user.clj
@@ -4,5 +4,7 @@
 
 (comment
   (k/run-all)
+  (k/run :shared)
+  (k/run :backend)
   (k/run 'backend.app.web.api.handlers.user-test)
   (k/run 'backend.app.web.api.handlers.user-test/create-and-get))
