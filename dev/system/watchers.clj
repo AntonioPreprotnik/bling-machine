1b2f56303de41e4bc895a5e29e874c0dca2ff3fd
enesj <enes.jakic@gmail.com>
2022-06-14 09:30:20 +0200

    Much simpler solution with one watcher.

--- dev/system/watchers.clj
+++ dev/system/watchers.clj
@@ -30,27 +30,19 @@
 (defn- edn-file? [{:keys [file]}]
   (re-matches #"[^.].*(\.edn)$" (.getName file)))
 
-(defn- system-watch-handler [ctx _]
-  (binding [*ns* *ns*]
-    ((:fn ctx) (:all ctx))
-    ctx))
+(defn- system-watch-handler [ctx e]
+  (let [all (edn-file? e)]
+    (binding [*ns* *ns*]
+      ((:fn ctx) all)
+      ctx)))
 
 (defn watch-backend
   "Automatically restarts the system if backend related files are changed."
   [reset-fn]
-  (hawk/watch! [{:paths   ["src/shared" "config/dev"]
-                 :handler (debounce #(if (edn-file? %2)
-                                       (system-watch-handler (merge %1 {:fn  reset-fn
-                                                                        :all true})
-                                                             %2)
-                                       (constantly nil)))}
-                {:paths   ["src/backend/" "src/shared"]
-                 :context (constantly  {:fn  reset-fn
-                                        :all false})
+  (hawk/watch! [{:paths   ["src/backend/" "src/shared" "config/dev"]
+                 :context (constantly  {:fn  reset-fn})
                  :filter  clojure-file?
-                 :handler (debounce #(if (edn-file? %2)
-                                       (constantly nil)
-                                       (system-watch-handler %1 %2)))}]))
+                 :handler (debounce #(system-watch-handler %1 %2))}]))
 
 (defn watch-frontend
   "Automatically re-builds frontend and re-renders browser page if frontend related files are changed."
