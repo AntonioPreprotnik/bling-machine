fb1bb2204ac18ef7586d84a243024def73ca297d
enesj <enes.jakic@gmail.com>
2022-06-17 13:48:34 +0200

    Added messages for stop and restart system

--- dev/system/core.clj
+++ dev/system/core.clj
@@ -4,7 +4,8 @@
    [clojure.tools.logging :refer [*tx-agent-levels*]]
    [clojure.tools.namespace.repl :refer [refresh refresh-all set-refresh-dirs]]
    [piotr-yuxuan.closeable-map :refer [closeable-map]]
-   [system.state :refer [dev-sys]]))
+   [system.state :refer [dev-sys]]
+   [taoensso.timbre :refer  [color-str info]]))
 
 (set-refresh-dirs "dev" "src" "resources")
 
@@ -23,9 +24,12 @@
 (defn restart-system
   "Stops system, refreshes changed namespaces in REPL and starts the system again."
   [all]
+  (info (color-str :blue "Stop system:"))
   (stop-system)
+  (info (color-str :blue "Restarting system:"))
   (if all
     (refresh-all :after 'system.core/start-system)
-    (refresh :after 'system.core/start-system)))
+    (refresh :after 'system.core/start-system))
+  (info (color-str :blue "System restarted.")))
 
 
