cf224aa0f18724547b417f069faa68daf9008616
marija <marija.pesa@verybogthings.com>
2022-06-29 10:19:56 +0200

    Add input and output schemas, prettify delete user component

--- src/app/frontend/ui/components/admin/delete_user.cljs
+++ src/app/frontend/ui/components/admin/delete_user.cljs
@@ -3,8 +3,13 @@
    [app.frontend.ui.components.button :refer [ButtonDefaul]]
    [helix.core :as hx :refer [$ <>]]
    [helix.dom :as d]
+   [keechma.next.helix.classified :refer [defclassified]]
    [keechma.next.helix.core :refer [dispatch use-sub with-keechma]]
    [keechma.next.helix.lib :refer [defnc]]))
+
+(defclassified DeleteUserCopy :div "text-center text-lg")
+(defclassified DeleteUserActionWrap :div "flex space-x-8")
+(def delete-button-style "w-full flex justify-center my-4")
 
 (defnc DeleteUser [props]
   {:wrap [with-keechma]}
@@ -12,12 +17,15 @@
         close-modal-delete-user              #(dispatch props :modal-delete-user :off)
         on-delete-user #(dispatch props :delete-user :on-delete-user)]
     (<>
-      (d/div {:class "flex justify-center text-lg"}
-             (str "Are you sure that want to delete user " first-name " " last-name "?"))
-      (d/div {:class "flex space-x-8"}
-             ($ ButtonDefaul {:additional-style "w-full flex justify-center mt-4"
-                              :label            "OK"
-                              :on-click         on-delete-user})
-             ($ ButtonDefaul {:additional-style "w-full flex justify-center mt-4"
-                              :label            "Cancel"
-                              :on-click         close-modal-delete-user})))))
+      ($ DeleteUserCopy
+        (d/p "Are you sure that want to delete user")
+        (d/p (str first-name " " last-name "?")))
+      ($ DeleteUserActionWrap
+        (map-indexed
+         (fn [idx {:keys [label on-click]}]
+           ($ ButtonDefaul {:key idx
+                            :additional-style delete-button-style
+                            :label            label
+                            :on-click         on-click}))
+         [{:label "OK" :on-click on-delete-user}
+          {:label "Cancel" :on-click close-modal-delete-user}])))))
