a58a782bfe7f6b471d09fca43e1a4fa6156a1232
marija <marija.pesa@verybogthings.com>
2022-07-12 15:45:06 +0200

    Add check-jwt handler, and transfer set-jwt to jwt controller

--- src/app/frontend/controllers/admin/login_form.cljs
+++ src/app/frontend/controllers/admin/login_form.cljs
@@ -1,9 +1,7 @@
 (ns app.frontend.controllers.admin.login-form
   (:require
-   [app.settings :refer [jwt-name]]
    [app.shared.schema :as schema]
    [com.verybigthings.funicular.controller :refer [command!]]
-   [hodgepodge.core :refer [local-storage set-item]]
    [keechma.malli-forms.core :as mf]
    [keechma.next.controller :as ctrl]
    [keechma.next.controllers.malli-form :as mfc]
@@ -15,9 +13,6 @@
 
 (def form (mf/make-form schema/registry :app.input.login nil))
 
-(defn set-jwt! [jwt]
-  (set-item local-storage jwt-name jwt))
-
 (def initial-login-form
   (pipeline! [value {:keys [meta-state* schema]}]
     (pp/swap! meta-state* mfc/init-form form)))
@@ -25,8 +20,6 @@
 (def submit-admin-credentials
   (-> (pipeline! [value {:keys [state*] :as ctrl}]
         (command! ctrl :api.session/login value)
-        (when-let [jwt (:jwt value)]
-          (set-jwt! jwt))
         (router/redirect! ctrl :router {:page "admin"})
         (rescue! [error]
           (pp/swap! state* assoc :submit-errors (ex-message error))))
--- src/app/frontend/controllers/jwt.cljs
+++ src/app/frontend/controllers/jwt.cljs
@@ -1,7 +1,8 @@
 (ns app.frontend.controllers.jwt
   (:require
    [app.settings :refer [jwt-name]]
-   [hodgepodge.core :refer [get-item local-storage remove-item]]
+   [com.verybigthings.funicular.controller :refer [command! get-command]]
+   [hodgepodge.core :refer [get-item local-storage remove-item set-item]]
    [keechma.next.controller :as ctrl]
    [keechma.next.controllers.pipelines :as pipelines]
    [keechma.next.controllers.router :as router]
@@ -9,17 +10,35 @@
 
 (derive :jwt ::pipelines/controller)
 
+(defn set-jwt! [jwt]
+  (set-item local-storage jwt-name jwt))
+
+(def set-jwt
+  (pipeline! [value {:keys [state*]}]
+    (let [payload (get-command value)
+          jwt (:jwt payload)]
+      (set-jwt! jwt)
+      (reset! state* jwt))))
+
 (def clear-jwt
   (pipeline! [value {:keys [state*] :as ctrl}]
     (remove-item local-storage jwt-name)
     (router/redirect! ctrl :router {:page "home"})))
 
+(def is-jwt-valid?
+  (-> (pipeline! [value {:keys [state*] :as ctrl}]
+        (let [jwt (get-item local-storage jwt-name)]
+          (pipeline! [value {:keys [state*]}]
+            (command! ctrl :api.session/check-jwt jwt)
+            (if (= :valid value)
+              (reset! state* jwt)
+              clear-jwt))))
+      (pp/set-queue :loading)))
+
 (def pipelines
-  {:keechma.on/start (-> (pipeline! [value {:keys [state*] :as ctrl}]
-                           (let [jwt (get-item local-storage jwt-name)]
-                             (reset! state* jwt)))
-                         (pp/set-queue :loading))
-   :log-out clear-jwt})
+  {:keechma.on/start is-jwt-valid?
+   :log-out clear-jwt
+   [:funicular/after :api.session/login] set-jwt})
 
 (defmethod ctrl/prep :jwt [ctrl]
   (pipelines/register ctrl pipelines))
