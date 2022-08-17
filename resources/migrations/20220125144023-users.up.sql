23b0a65b1bd5c380eb0946ae72b0de35964e9d2a
TheGamePrep <preprotnik.novi@gmail.com>
2022-08-17 23:11:10 +0200

    Improve existing migrations

--- resources/migrations/20220125144023-users.up.sql
+++ resources/migrations/20220125144023-users.up.sql
@@ -3,15 +3,14 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS citext;
 --;;
 CREATE TABLE users (
-  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  id uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
   first_name character varying(255) NOT NULL,
   last_name character varying(255) NOT NULL,
   email citext NOT NULL,
   is_admin BOOLEAN DEFAULT FALSE,
   password_hash VARCHAR(255) NOT NULL,
   inserted_at timestamp without time zone DEFAULT NOW(),
-  updated_at timestamp without time zone DEFAULT NOW(),
-  PRIMARY KEY (id)
+  updated_at timestamp without time zone DEFAULT NOW()
 );
 
 --;;
--- resources/migrations/20220816110658-add-currencies-table.down.sql
+++ resources/migrations/20220816110658-add-currencies-table.down.sql
@@ -1,3 +1,4 @@
+DROP UNIQUE INDEX name_to_date_index;
 DROP TABLE currencies;
 --;;
 DROP EXTENSION IF EXISTS "uuid-ossp";
